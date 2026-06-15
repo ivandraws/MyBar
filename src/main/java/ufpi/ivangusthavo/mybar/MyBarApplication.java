34fd5c35877deea6a28cda764f822d324b905aff
ivandraws <62605967+ivandraws@users.noreply.github.com>
2026-06-15 16:35:00 -0300

    First Commit

---
+++ src/main/java/ufpi/ivangusthavo/mybar/MyBarApplication.java
@@ -0,0 +1,13 @@
+package ufpi.ivangusthavo.mybar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ufpi/ivangusthavo/mybar/MyBarApplicationTests.java
@@ -0,0 +1,13 @@
+package ufpi.ivangusthavo.mybar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
