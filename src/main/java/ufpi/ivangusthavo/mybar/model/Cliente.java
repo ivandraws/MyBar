370687c5ef985b24623133fcf80230a083075bd1
ivandraws <62605967+ivandraws@users.noreply.github.com>
2026-06-16 18:34:51 -0300

    Incluido Gets e Setters do Cliente

--- src/main/java/ufpi/ivangusthavo/mybar/model/Cliente.java
+++ src/main/java/ufpi/ivangusthavo/mybar/model/Cliente.java
@@ -8,6 +8,51 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private String cpf;
+    private String nome;
+    private String telefone;
+    private Sexo sexo;
+    public Cliente(){
 
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
 }
---
+++ src/main/java/ufpi/ivangusthavo/mybar/model/Sexo.java
@@ -0,0 +1,6 @@
+package ufpi.ivangusthavo.mybar.model;
+
+public enum Sexo {
+    MASCULINO,
+    FEMININO
+}
