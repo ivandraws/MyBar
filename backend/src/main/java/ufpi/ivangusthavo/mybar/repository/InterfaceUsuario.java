8929c37c54151c95ed0c79d3b08e14fbec7e487b
ivandraws <62605967+ivandraws@users.noreply.github.com>
2026-06-22 17:59:45 -0300

    Alterado de CrudRepository para JpaRepository

--- backend/src/main/java/ufpi/ivangusthavo/mybar/repository/InterfaceUsuario.java
+++ backend/src/main/java/ufpi/ivangusthavo/mybar/repository/InterfaceUsuario.java
@@ -1,7 +1,7 @@
 package ufpi.ivangusthavo.mybar.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import ufpi.ivangusthavo.mybar.model.Usuario;
 
-public interface InterfaceUsuario extends CrudRepository<Usuario, Integer> {
+public interface InterfaceUsuario extends JpaRepository<Usuario, Integer> {
 }
