b3fdb3cff17d545760ac35f7037530b30d141408
gusthavo <gusthavoeduardolima615@gmail.com>
2026-06-25 12:44:55 -0300

    feat: finished dashborad

---
+++ backend/src/main/java/ufpi/ivangusthavo/mybar/model/Conta.java
@@ -0,0 +1,14 @@
+package ufpi.ivangusthavo.mybar.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Conta {
+    public int numero;
+    
+    public Conta(){
+
+    }
+}
--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,8 +1,8 @@
 
-
 function carregarUsuario(nome, cargo, inicial) {
     document.getElementById('user-name').innerText = nome;
     document.getElementById('user-role').innerText = cargo;
     document.getElementById('user-avatar').innerText = inicial;
 }
+
 
